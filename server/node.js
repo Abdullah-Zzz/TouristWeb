daf10f3250771fc0abb006af8a7e35cf6b963bef
abdullah-zzz <ak0344494@gmail.com>
2024-07-10 19:06:26 +0500

    some changes

--- server/node.js
+++ server/node.js
@@ -2,14 +2,13 @@ const DB = require("./db")
 const express=require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
-const path = require('path')
 require('dotenv').config();
 const userRoute = require("./routes/user")
 const tripRoute = require("./routes/trips")
 
 
 const app=express();
-const port=8080;
+const port=process.env.PORT || 8080;
 
 app.use(cookieParser())
 app.use(express.json())
@@ -20,11 +19,6 @@ app.use(cors({
 
 app.use('/users', userRoute)
 app.use('/api', tripRoute)
-
-app.get("/", (req, res) => {
-    app.use(express.static(path.resolve(__dirname, "client", "dist")));
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-    });
 
 app.listen(port,() =>{
     console.log("running...")
