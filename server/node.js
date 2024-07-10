2f9e562356225d27c3ec18eab4008772fb2098eb
abdullah-zzz <ak0344494@gmail.com>
2024-07-10 19:04:10 +0500

    some changes

--- server/node.js
+++ server/node.js
@@ -3,6 +3,7 @@ const express=require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const path = require('path')
+require('dotenv').config();
 const userRoute = require("./routes/user")
 const tripRoute = require("./routes/trips")
 
